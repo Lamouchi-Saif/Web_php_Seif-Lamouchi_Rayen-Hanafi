50bc7b6b0ca66905eec423bb88d6d895f123effe
RayenHanafi <rayen.hanafi@insat.ucar.tn>
2025-04-07 10:08:54 +0100

    organizing commit

---
+++ Session/index.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'SessionManager'
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <link
+    rel="stylesheet"
+    href="./node_modules/bootstrap/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="index2.css" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Title</Title>
+  </title>
+</head>
+
+<body
+  class="bg-light d-flex justify-content-center align-items-center vh-100">
+  <div class="container">
+    <div class="card shadow p-4">
+      <h2 class="text-center mb-4">Title</h2>
+      <div class="mb-3">
+
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+      </div>
+
+      <div class="text-center mt-4">
+
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
